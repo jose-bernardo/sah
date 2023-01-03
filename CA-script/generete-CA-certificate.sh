6fad65a21865e15feb906c9b71fa19c736259c98
Gonçalo Carvalho <gas.carvalho@campus.fct.unl.pt>
2023-01-03 15:13:18 +0000

    Deletes cert and key files

--- CA-script/generete-CA-certificate.sh
+++ CA-script/generete-CA-certificate.sh
@@ -7,12 +7,11 @@ if [ $# -ne 1 ]; then
 fi
 
 ca_name=$1
-ca_dir="./CA_$ca_name"
+ca_dir="./$ca_name"
 
 mkdir "$ca_dir"
 cd "$ca_dir"
 
 openssl genrsa -out ca-key.pem 4096
 
-openssl req -x509 -new -nodes -key ca-key.pem -sha256 -days 3600 -out ca.pem -config sah.config
-
+openssl req -x509 -new -nodes -key ca-key.pem -sha256 -days 3600 -out ca.pem -subj "/CN=$ca_name"
