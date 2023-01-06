df18cf1ebad699820240e3b5c43b6ae734a59f86
pexas <josebernardo@tecnico.ulisboa.pt>
2023-01-06 16:25:25 +0000

    fix database

--- database/populate.sql
+++ database/populate.sql
@@ -1,3 +1,8 @@
+-- Add medical specialties
+INSERT INTO MedicalSpecialty (name) VALUES ("Cardiology");
+INSERT INTO MedicalSpecialty (name) VALUES ("Dermatology");
+INSERT INTO MedicalSpecialty (name) VALUES ("Orthopedy");
+
 INSERT INTO Patients (nhs, email, name, password) VALUES ("123456789", "pexas@yahoo.copm", "José","password");
 INSERT INTO Patients (nhs, email, name, password) VALUES ("987654321", "NNate@sah.com.pt", "Nuno","password2");
 INSERT INTO Patients (nhs, email, name, password) VALUES ("123987456", "gas@xd.pt", "Gonçalo","password3");
